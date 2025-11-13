5169f3712289ee5573cc1da7768470c12bafe263
vishnue.bn <vishnue.bn@pitsolutions.com>
2025-11-13 11:52:39 +0530

    fix: Change path of reg,login

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Register } from "./auth/pages/pages/register/register";
-import { Login } from "./auth/pages/pages/login/login";
+import { Register } from './auth/pages/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Register, Login],
+  imports: [RouterOutlet, Register],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
